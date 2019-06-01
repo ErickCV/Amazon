00029d5f7641999f4643a228c42c48df6f6bb661
ErickCV <16030835@itcelaya.edu.mx>
2019-06-01 17:47:19 -0500

    proyecto sin .idea

--- src/sample/Vistas/Main.java
+++ src/sample/Vistas/Main.java
@@ -1,21 +1,8 @@
 package sample.Vistas;
 
-import com.jfoenix.controls.JFXButton;
 import javafx.application.Application;
-import javafx.event.Event;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import sample.FormatToController.AddPackageController;
 import sample.Modelos.ConexionBD;
-
-import java.awt.event.MouseEvent;
-import java.io.IOException;
 
 public class Main extends Application {
 
