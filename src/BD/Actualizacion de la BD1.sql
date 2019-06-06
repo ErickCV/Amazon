bf2d4d489c407d22ab61d8fc79eeebcd7a340de1
Fco Javier <16030971@itcelaya.edu.mx>
2019-06-06 09:10:31 -0500

    Actualizacion del documento nuevo del SQL

--- src/BD/Actualizacion de la BD1.sql
+++ src/BD/Actualizacion de la BD1.sql
@@ -1,6 +1,8 @@
 use amazonV3
 select * from Product
 insert into Product values
+	('print Brother',3,'DCP-L2540DW multifuncional',2999,'/sample/imagenes/brother.jpg'),
+	('Cooler Liquid',1,'Enfriamento Cooler Master',4347.88,'/sample/imagenes/coler.jpg'),
 	('Folder',2,'Folder amarillo',3.00,'/sample/imagenes/folder.jpg'),
 	('Protectores',2,'Protector transparente',3.00,'/sample/imagenes/protector.jpg'),
 	('Memoria RAM',1,'DDR4 de 8 GB',1100,'/sample/imagenes/ram.jpg'),
@@ -8,7 +10,8 @@ insert into Product values
 	('Laptop Dell',3,'8GB RAM Intel i7, GTX 1060',18500,'/sample/imagenes/laptop.jpg'),
 	('Lapiz',2,'Lapiz HB',4.50,'/sample/imagenes/lapiz.jpg')
 
-
+select *
+	from Category
 create procedure filtro
 	@categoria VARCHAR(15)
 as
