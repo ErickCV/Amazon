1e00ec01be70353eb1ae7bada0d40889c023501c
ErickCV <16030835@itcelaya.edu.mx>
2019-06-10 09:58:21 -0500

    Restauracion

--- src/sample/Modelos/ConexionBD.java
+++ src/sample/Modelos/ConexionBD.java
@@ -6,23 +6,21 @@ import java.sql.SQLException;
 
 public class ConexionBD
 {
-    public static Connection cn = null;
-    private static String host="localhost";
-    private  static String BD="amazonV3";
-    private  static String User="root",password="yosolosequenosenada";
+    public Connection cn = null;
     public Connection getConectar()
     {
-
         return cn;
     }
     public ConexionBD()
     {
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-            cn = DriverManager.getConnection("jdbc:mysql://"+ host +":3306/" + BD, User, password);
-        }
-        catch (Exception e){
-            System.out.println(e.getMessage());
+        try
+        {
+            String connectionUrl = "jdbc:sqlserver://;database=AmazonV3; user=Erick; password=itgd; integratedSecurity=true;";
+            cn = DriverManager.getConnection(connectionUrl);
+            System.out.println("Conectado");
+        } catch (SQLException ex)
+        {
+            System.out.println("Error: "+ex);
         }
     }
 }
